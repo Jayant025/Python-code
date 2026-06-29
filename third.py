b24304d04ff14e96036672d8ffaac9512087d555
JAYANT <rajawatjayant66@gmail.com>
2026-06-29 22:13:04 +0530

    new commit

---
+++ third.py
@@ -0,0 +1,12 @@
+a=int(input("enter number:"))
+b=int(input("enter number:"))
+c=int(input("enter number:"))
+print(a)
+print(b)
+print(c)
+if a>b and a>c :
+ print("a is largest")
+elif b>c and b>a:
+    print("b is largest")
+else:
+   print("c is largest")
