d75a837cd2872f953488de6602d7fd82657cb132
JAYANT <rajawatjayant66@gmail.com>
2026-06-29 22:32:25 +0530

    new commit

---
+++ 5th.py
@@ -0,0 +1,15 @@
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+oper=int(input("enter number:"))
+if oper==1:
+    c=a+b
+    print(c)
+elif oper==2:
+    c=a-b
+    print(c)
+elif oper == 3:
+    c=a*b
+    print(c)
+else:
+    c=a/b
+    print(c)
