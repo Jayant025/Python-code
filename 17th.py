6de02470d1f58acc5c27392f79b54f5303661e35
JAYANT <rajawatjayant66@gmail.com>
2026-07-07 12:19:01 +0530

    new commit

---
+++ 17th.py
@@ -0,0 +1,19 @@
+passw=input("enter password:")
+upper=0
+lower=0
+digit=0
+for ch in passw:
+ if ch>='A' and ch<='Z':
+  upper+=1
+ elif ch>='a' and ch<='z':
+  lower+=1
+ else:
+  digit+=1
+
+print("Uppercase letters:", upper)
+print("Lowercase letters:", lower)
+print("Digits:", digit)
+if upper>0 and lower>0 and digit>0:
+ print("strong password")
+else:
+ print("week password")
